abfacca694a024f1961a35e7fdde17025eb73f70
Lukáš Chábek <lukaschabek@gmail.com>
2018-04-04 15:35:45 +0200

    Basic project structure

---
+++ README.md
@@ -0,0 +1,2 @@
+# Java Block Scheme
+Application written in Java that allows easily create block schemes for calculations.
---
+++ src/BlockSchemeApp.java
@@ -0,0 +1,19 @@
+package block_scheme;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class BlockSchemeApp extends Application
+{
+	public static void main(String[] args)
+	{
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage)
+	{
+
+	}
+}
