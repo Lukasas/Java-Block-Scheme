f0b0198cfeb193000813c95571fcc6b92b7b0824
Lukáš Chábek <lukaschabek@gmail.com>
2018-04-16 21:03:55 +0200

    Added basic window
    Changed build run to actually run the project and test to test the project.

--- src/BlockSchemeApp.java
+++ src/BlockSchemeApp.java
@@ -1,32 +1,33 @@
 package blockscheme;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+// import javafx.scene.control.Pos;
 import javafx.stage.Stage;
 
-/*
 public class BlockSchemeApp extends Application
 {
 	public static void main(String[] args)
 	{
+
 		System.out.println("Hello World.");
 
-		//launch(args);
+		launch(args);
 	}
 
 	@Override
 	public void start(Stage stage)
 	{
+		stage.setTitle("Block Scheme Application");
 
+		GridPane grid = new GridPane();
+		// grid.setAligment(Pos.CENTER);
+		grid.setHgap(0);
+		grid.setVgap(0);
+
+		Scene scene = new Scene(grid, 300, 275);
+		stage.setScene(scene);
+		stage.show();
 	}
-}*/
-
-public class BlockSchemeApp
-{
-	public static void main(String[] args)
-	{
-		System.out.println("Hello World.");
-
-		//launch(args);
-	}
-
 }
